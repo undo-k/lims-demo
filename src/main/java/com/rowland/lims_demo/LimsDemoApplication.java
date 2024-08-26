64856157205f5a76ed6c5e5a70386a0df8001fb4
Jordan Rowland <46544691+undo-k@users.noreply.github.com>
2024-08-26 16:58:05 -0400

    Spring Initializer Commit

---
+++ src/main/java/com/rowland/lims_demo/LimsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rowland.lims_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimsDemoApplication.class, args);
+	}
+
+}
