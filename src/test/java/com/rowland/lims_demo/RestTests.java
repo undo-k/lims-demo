65a1102052259e51faa4f4fe871b078b0cd8b583
Jordan Rowland <46544691+undo-k@users.noreply.github.com>
2024-08-28 12:47:40 -0400

    added update rest tests & json serialization cleanup

--- src/test/java/com/rowland/lims_demo/RestTests.java
+++ src/test/java/com/rowland/lims_demo/RestTests.java
@@ -1,6 +1,7 @@
 package com.rowland.lims_demo;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,25 +26,73 @@ public class RestTests {
     @Autowired
     private PatientRepository repo;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    String createPatientURI = "/api/patients/create";
+    String updatePatientURI = "/api/patients/update";
+    String getPatientURI = "/api/patients/";
+
     @Test
-    public void PutThenGetPatient() throws Exception {
+    public void CreateThenGetPatient() throws Exception {
 
         Patient john = new Patient("John", "Smith", "1963-11-22", "M");
 
-        String patientJson = String.format("{\"firstName\": \"%s\", \"lastName\": \"%s\", \"dateOfBirth\": \"%s\", \"sex\": \"%s\"}", john.getFirstName(), john.getLastName(), john.getDateOfBirth().toString(), john.getSex());
-        mvc.perform(MockMvcRequestBuilders.put("/api/patients/create").contentType(MediaType.APPLICATION_JSON).content(patientJson));
-        mvc.perform(MockMvcRequestBuilders.put("/api/patients/create").contentType(MediaType.APPLICATION_JSON).content(patientJson));
-        mvc.perform(MockMvcRequestBuilders.put("/api/patients/create").contentType(MediaType.APPLICATION_JSON).content(patientJson));
-        mvc.perform(MockMvcRequestBuilders.put("/api/patients/create").contentType(MediaType.APPLICATION_JSON).content(patientJson));
-        mvc.perform(MockMvcRequestBuilders.put("/api/patients/create").contentType(MediaType.APPLICATION_JSON).content(patientJson));
+        objectMapper.writeValueAsString(john);
 
-        mvc.perform(MockMvcRequestBuilders.get("/api/patients/1")
+        String patientJson = objectMapper.writeValueAsString(john);
+        mvc.perform(MockMvcRequestBuilders.put(createPatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+        mvc.perform(MockMvcRequestBuilders.put(createPatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+        mvc.perform(MockMvcRequestBuilders.put(createPatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+        mvc.perform(MockMvcRequestBuilders.put(createPatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+        mvc.perform(MockMvcRequestBuilders.put(createPatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+
+        mvc.perform(MockMvcRequestBuilders.get(getPatientURI + "1")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content()
                         .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("firstName").value(john.getFirstName()))
                 .andExpect(jsonPath("lastName").value(john.getLastName()));
-        mvc.perform(MockMvcRequestBuilders.get("/api/patients/2").contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isNotFound());
+        mvc.perform(MockMvcRequestBuilders.get(getPatientURI + "2").contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isNotFound());
     }
+
+    @Test
+    public void UpdateThenGetPatient() throws Exception {
+
+        Patient john = new Patient("John", "Smith", "1963-11-22", "M");
+
+
+        String patientJson = objectMapper.writeValueAsString(john);
+        mvc.perform(MockMvcRequestBuilders.put(createPatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+
+        john.setAddress("Wallaby Way");
+        patientJson = objectMapper.writeValueAsString(john);
+        mvc.perform(MockMvcRequestBuilders.put(updatePatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+
+        john.setAddress("Spain");
+        patientJson = objectMapper.writeValueAsString(john);
+        mvc.perform(MockMvcRequestBuilders.put(updatePatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+
+        john.setAddress("sdfdfgdfgdf56yugh5itdfg3w");
+        patientJson = objectMapper.writeValueAsString(john);
+        mvc.perform(MockMvcRequestBuilders.put(updatePatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+
+        john.setAddress("9c7c5fbb210b50e97c27a20583c099538f594f2422925654055fdb2484edb6f6");
+        patientJson = objectMapper.writeValueAsString(john);
+        mvc.perform(MockMvcRequestBuilders.put(updatePatientURI).contentType(MediaType.APPLICATION_JSON).content(patientJson));
+
+        mvc.perform(MockMvcRequestBuilders.get(getPatientURI + "1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("firstName").value(john.getFirstName()))
+                .andExpect(jsonPath("lastName").value(john.getLastName()))
+                .andExpect(jsonPath("address").value("9c7c5fbb210b50e97c27a20583c099538f594f2422925654055fdb2484edb6f6"));
+
+        mvc.perform(MockMvcRequestBuilders.get(getPatientURI + "2").contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+
 }
